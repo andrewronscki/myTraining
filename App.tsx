77e69fd1d2ba5a391d6edfcadfbfffd499c48b33
André Wronscki <andre.wronscki@totalvoice.com.br>
2020-02-07 13:07:28 -0300

    Commit inicial

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Esse é o App do MyTraining</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
