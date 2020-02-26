1457b90be9f612adb10c0ba0d17329d1382ccb63
André Wronscki Ricardo <andre@MacBook-Pro-de-Andre.local>
2020-02-25 21:12:53 -0300

    criacao de conteudo da page main

--- pages/Main/index.js
+++ pages/Main/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, ContainerItems, Item, TextItem } from './styles';
 
 export default function Main() {
   return (
     <>
       <Header />
-      <Container />
+      <Container>
+        <ContainerItems>
+          <Item onClick={() => {}}><TextItem>Treinos</TextItem></Item>
+        </ContainerItems>
+
+        <ContainerItems>
+          <Item onClick={() => {}}><TextItem>Medidas</TextItem></Item>
+        </ContainerItems>
+
+        <ContainerItems>
+          <Item onClick={() => {}}><TextItem>Dieta</TextItem></Item>
+        </ContainerItems>
+        
+       
+      </Container>
     </>
   );
 }
--- pages/Main/styles.js
+++ pages/Main/styles.js
@@ -3,4 +3,34 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   background: #333;
   height: 100%;
+
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContainerItems = styled.View`
+  padding: 30px;
+  margin-bottom: 15px;
+
+  height: 80px;
+  width: 100%;
+`;
+
+export const Item = styled.TouchableOpacity`
+  background: #333;
+  border: 2px solid #FFE700;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+export const TextItem = styled.Text`
+  padding: 30px 0 30px;
+
+  color: #FFE700;
+  font-size: 22px;
 `;
